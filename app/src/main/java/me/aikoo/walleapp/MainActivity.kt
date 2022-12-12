af9e9045ff6a71c0150d622e5e00523886924574
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-12-12 11:45:41 +0100

    Base bluetooth connection

--- app/src/main/java/me/aikoo/walleapp/MainActivity.kt
+++ app/src/main/java/me/aikoo/walleapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package me.aikoo.walleapp
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,11 +14,11 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import java.util.*
 import kotlin.concurrent.scheduleAtFixedRate
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var timer: Timer;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    @SuppressLint("SetTextI18n")
+    fun connectBluetooth(view: View) {
+        val status = findViewById<TextView>(R.id.statusText)
+        status.text = "Status: Connecting to Wall.E..."
+
+        val btManager: BluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val btAdapter: BluetoothAdapter? = btManager.adapter
+        val deviceAddress = "98:D3:51:FE:45:1A"
+        val device: BluetoothDevice? = btAdapter?.getRemoteDevice(deviceAddress)
+        val permission = packageManager.checkPermission(Manifest.permission.BLUETOOTH, packageName);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            if (device != null) {
+                val socket: BluetoothSocket? = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                socket?.connect()
+                if (socket?.isConnected == true) {
+                    status.text = "Status: Connected to Wall.E!"
+                } else {
+                    status.text = "Status: Failed to connect to Wall.E! a "
+                }
+            } else {
+                status.text = "Status: Failed to connect to Wall.E! b"
+            }
+        } else {
+            status.text = "Status: Failed to connect to Wall.E! Missing permissions!"
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
     fun startTimer(view: View) {
         val countdownTextView = findViewById<TextView>(R.id.countdown)
         val buttonView = findViewById<TextView>(R.id.button1)
